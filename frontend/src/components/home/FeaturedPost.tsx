a9cf41167aaf2d127b0c65bdd78e1882c1c089ba
Huntertk <taufik.khan65@gmail.com>
2024-12-03 15:32:27 +0530

    one post removed

--- frontend/src/components/home/FeaturedPost.tsx
+++ frontend/src/components/home/FeaturedPost.tsx
@@ -43,20 +43,6 @@ const FeaturedPost = () => {
                     <h1>9 Best Ways to Celebrate Summer in Thailand</h1>
                 </div>
             </div>
-
-            <div className="featured_post">
-                <div className="feature_post_img">
-                    <img src="https://i.postimg.cc/W4B71NYb/pexels-pixabay-208444.jpg" alt="" />
-                </div>
-
-                <div className="featured_post_content">
-                    <div className="featured_post_time_category">
-                        <p>thailand</p>
-                        <p>1 hours ago</p>
-                    </div>
-                    <h1>9 Best Ways to Celebrate Summer in Thailand</h1>
-                </div>
-            </div>
         </div>
     </div>
   )
