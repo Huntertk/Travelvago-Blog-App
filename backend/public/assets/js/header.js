2deed1cdc42307ac3e1e5542c401f284246b239b
Huntertk <taufik.khan65@gmail.com>
2024-12-13 21:19:07 +0530

    navbar bug fixed

--- backend/public/assets/js/header.js
+++ backend/public/assets/js/header.js
@@ -2,5 +2,13 @@ const navToggleIconEl = document.querySelector('#nav_toggle_div');
 const navLinksEl = document.querySelector('#nav_links');
 
 navToggleIconEl.addEventListener('click', () => {
-    navLinksEl.classList.toggle('nav_active');
+    if (navLinksEl.classList.contains('nav_active')) { 
+        navLinksEl.classList.remove('nav_active');
+        document.querySelector('#closeIcon').style.display = 'none'
+        document.querySelector('#openIcon').style.display = 'block'
+    } else { 
+        navLinksEl.classList.add('nav_active');
+        document.querySelector('#closeIcon').style.display = 'block'
+        document.querySelector('#openIcon').style.display = 'none'
+    }
 })
