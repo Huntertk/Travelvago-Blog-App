5ecdd54854bbe5c9624972fb41df6d558f38ad00
Huntertk <taufik.khan65@gmail.com>
2024-12-13 18:43:41 +0530

    header active logic added

--- backend/src/routes/viewRoutes.ts
+++ backend/src/routes/viewRoutes.ts
@@ -4,7 +4,9 @@ const router = Router();
 
 router.get('/', (req:Request, res:Response, next:NextFunction) => {
     try {
-        return res.render('index.ejs')
+        return res.render('index.ejs', {
+            pageActive:"home"
+        })
     } catch (error) {
         return next(error);   
     }
