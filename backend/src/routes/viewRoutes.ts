54c238c557491294b6e6df12a08b16a41aa147e6
Huntertk <taufik.khan65@gmail.com>
2024-12-13 23:25:42 +0530

    footer created

--- backend/src/routes/viewRoutes.ts
+++ backend/src/routes/viewRoutes.ts
@@ -4,8 +4,10 @@ const router = Router();
 
 router.get('/', (req:Request, res:Response, next:NextFunction) => {
     try {
+        const currentYear = new Date(Date.now()).getFullYear();
         return res.render('index.ejs', {
-            pageActive:"home"
+            pageActive:"home",
+            currentYear
         })
     } catch (error) {
         return next(error);   
