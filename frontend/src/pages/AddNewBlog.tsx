61c22bd97c4d4eb5e9fdb77fc34ae9b706604385
Huntertk <taufik.khan65@gmail.com>
2024-12-18 01:37:09 +0530

    adding quill height

--- frontend/src/pages/AddNewBlog.tsx
+++ frontend/src/pages/AddNewBlog.tsx
@@ -127,7 +127,7 @@ const AddNewBlog = () => {
                 </select>
             </div>
             <div className="add_new_blog_text_editor">
-                <ReactQuill theme="snow" value={content} ref={editorRef} onChange={setContent} placeholder='Write blog here....'/>
+                <ReactQuill className='textEditorQuill' theme="snow" value={content} ref={editorRef} onChange={setContent} placeholder='Write blog here....'/>
             </div>
             <button className='create_btn' onClick={handleFormSubmit} disabled={createBlogLoading}>{createBlogLoading ? <VscLoading className='loading' /> : "Create Blog"}</button>
         </div>
