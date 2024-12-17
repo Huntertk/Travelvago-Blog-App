cf975ee8f3272f4f4dfded73e3ca8cd2e8b13bd8
Huntertk <taufik.khan65@gmail.com>
2024-12-17 18:50:09 +0530

    blog details page created

--- backend/src/routes/viewRoutes.ts
+++ backend/src/routes/viewRoutes.ts
@@ -17,5 +17,23 @@ router.get('/', async (req:Request, res:Response, next:NextFunction) => {
         return next(error);   
     }
 });
+router.get('/blog/:slug', async (req:Request, res:Response, next:NextFunction) => {
+    try {
+        const params = req.params
+        const blog = await Blog.findOne({slug:params.slug});
+        if(!blog){
+            return res.redirect('/')
+        }
+        
+        const currentYear = new Date(Date.now()).getFullYear();
+        return res.render('blogPage.ejs', {
+            pageActive:"blog",
+            currentYear,
+            blog
+        })
+    } catch (error) {
+        return next(error);   
+    }
+});
 
 export default router;
