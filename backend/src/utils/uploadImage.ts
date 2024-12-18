2fc7d17da8a3e9f87bef262442fa0822cf49e0a6
Huntertk <taufik.khan65@gmail.com>
2024-12-18 17:35:33 +0530

    one minor error fixed while uploading image to cloudinary

--- backend/src/utils/uploadImage.ts
+++ backend/src/utils/uploadImage.ts
@@ -4,5 +4,5 @@ export const uploadImage = async (imageFile: Express.Multer.File) => {
     const b64 = Buffer.from(imageFile.buffer).toString("base64");
     let dataURI = "data:" + imageFile.mimetype + ";base64," + b64;
     const res = await cloudinary.v2.uploader.upload(dataURI);
-    return res.url;
+    return res.secure_url;
 }
