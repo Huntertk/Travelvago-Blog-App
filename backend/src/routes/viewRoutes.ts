60e92a61e2ce1eb3d35c6fb4a002669a8bfa7b6d
Huntertk <taufik.khan65@gmail.com>
2024-12-31 15:10:11 +0530

    adding logic to exclud existting blog post

--- backend/src/routes/viewRoutes.ts
+++ backend/src/routes/viewRoutes.ts
@@ -20,7 +20,8 @@ router.get('/', async (req:Request, res:Response, next:NextFunction) => {
 router.get('/blog/:slug', async (req:Request, res:Response, next:NextFunction) => {
     try {
         const params = req.params;
-        const randomBlog = await Blog.aggregate([ { $sample: { size: 3 } } ]);
+        // const randomBlog = await Blog.aggregate([ { $sample: { size: 3 } } ]);
+        const randomBlog = await Blog.find({slug:{$ne:params.slug}});
         const blog = await Blog.findOne({slug:params.slug});
         if(!blog){
             return res.redirect('/')
@@ -31,7 +32,7 @@ router.get('/blog/:slug', async (req:Request, res:Response, next:NextFunction) =
             pageActive:"blog",
             currentYear,
             blog,
-            randomBlog
+            randomBlog:randomBlog.slice(0,3)
         })
     } catch (error) {
         return next(error);   
